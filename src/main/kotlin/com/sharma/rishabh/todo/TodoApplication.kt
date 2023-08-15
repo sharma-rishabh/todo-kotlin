2bc716d8db832b7a1a578b005e3e119fdaa6c2ee
Risabh Sharma <99526730+sharma-rishabh@users.noreply.github.com>
2023-08-15 14:59:52 +0530

    Initial commit

---
+++ src/main/kotlin/com/sharma/rishabh/todo/TodoApplication.kt
@@ -0,0 +1,11 @@
+package com.sharma.rishabh.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoApplication>(*args)
+}
